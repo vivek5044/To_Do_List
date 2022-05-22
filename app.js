aa55b35858a2b950cd6c56eecb81ba5a68887752
vivek5044 <vivekshukla509@gmail.com>
2022-05-22 16:01:48 +0530

    added header and footer files

--- app.js
+++ app.js
@@ -2,33 +2,48 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
-var items = ["But food", "Cook food", "Eat food"];
+let items = ["But food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    let today = new Date();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-    res.render("list", {kindOfDay: day, newListItems: items});
+    let day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.render("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
 
+})
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    // console.log(item);
+    res.redirect("/work");
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
 })
 
 
